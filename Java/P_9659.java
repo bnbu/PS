3989b006731b46f32280ae222d051c2c5c616725
bnbu <63402731+bnbu@users.noreply.github.com>
2021-08-31 03:07:27 +0900

    Create P_9659.java

---
+++ Java/P_9659.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		long n = scan.nextLong();
+		System.out.println(n % 2 == 1 ? "SK" : "CY");
+	}
+}
+// 2020-09-02 20:06 해결
+// 홀수 짝수 싸움 에반데3
