6c0aa0e11b9e43d13be4f3d074fadcf93daf949c
jsh031613 <jsh031613@naver.com>
2023-05-25 15:35:56 +0900

    Update Merge_Sort_Tree.java

--- template/Java/Data_Struct/Merge_Sort_Tree.java
+++ template/Java/Data_Struct/Merge_Sort_Tree.java
@@ -1,9 +1,10 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-	static long cnt;
-	static ArrayList<Integer>[] tree;
-	static int lowerBoundSearch(ArrayList<Integer> arr, int k) {
+	static int n;
+	static int[] arr;
+	static ArrayList<Integer>[] mergeTree;
+	static int lowerBound(ArrayList<Integer> arr, int k) {
     	int s = 0,
     		e = arr.size();
         while (s < e) {
@@ -13,46 +14,65 @@ public class Main {
         }
         return s;
 	}
-	static ArrayList<Integer> makeMergeSortTree(int start, int end, int node) {
-		if (start == end) return tree[node];
+	static int upperBound(ArrayList<Integer> arr, int k) {
+		int s = 0,
+			e = arr.size();
 		
+		while(s < e) {
+			int m = (s + e) / 2;
+			if(arr.get(m) <= k) s = m + 1;
+			else e = m;
+		}
+		return s;
+	}
+	static ArrayList<Integer> makeMergeSortTree(int start, int end, int node) {
+		if (start == end) {
+			mergeTree[node].add(arr[start]);
+			return mergeTree[node];
+		}
 		int mid = (start + end) / 2;
 		ArrayList<Integer> left = makeMergeSortTree(start, mid, 2 * node),
-							right = makeMergeSortTree(mid + 1, end, 2 * node + 1);
-		
+				right = makeMergeSortTree(mid + 1, end, 2 * node + 1);
+
 		int last = 0, n = 0;
 		for (int l : left) {
-			n = lowerBoundSearch(right, l);
-			cnt += n;
-			
-			for (int i = last; i < n; i++) tree[node].add(right.get(i));
-			tree[node].add(l);
+			n = lowerBound(right, l);
+			for (int i = last; i < n; i++) mergeTree[node].add(right.get(i));
+			mergeTree[node].add(l);
 			last = n;
 		}
-		for (int i = last; i < right.size(); i++) tree[node].add(right.get(i));
+		for (int i = last; i < right.size(); i++) mergeTree[node].add(right.get(i));
 		
-		return tree[node];
+		return mergeTree[node];
 	}
-	static int findNode(int start, int end, int node, int idx) {
-		if (start == end) return node;
-		
+	public static int get(int start, int end, int node, int left, int right, int k) {
+		if (right < start || end < left) return 0;
+		if (left <= start && end <= right) {
+			int ret = upperBound(mergeTree[node], k);
+			return mergeTree[node].size() - ret;
+		}
 		int mid = (start + end) / 2;
-		if (idx <= mid) return findNode(start, mid, 2 * node, idx);
-		else return findNode(mid + 1, end, 2 * node + 1, idx);
+		return get(start, mid, 2*node, left, right, k) + get(mid + 1, end, 2*node + 1, left, right, k);
 	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
 		int n = Integer.parseInt(br.readLine());
 		
-		tree = new ArrayList[4*n];
-		for (int i = 1; i < tree.length; i++) tree[i] = new ArrayList<>();
+		mergeTree = new ArrayList[4*n];
+		for (int i = 1; i < 4*n; i++) mergeTree[i] = new ArrayList<>();
 		
+		arr = new int[n + 1];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= n; i++) tree[findNode(1, n, 1, i)].add(Integer.parseInt(st.nextToken()));
+		for (int i = 1; i <= n; i++) arr[i] = Integer.parseInt(st.nextToken());
 		makeMergeSortTree(1, n, 1);
-		System.out.println(cnt);
-	}
+		
+		int m = Integer.parseInt(br.readLine());
+		while (m-- > 0) { 
+			st = new StringTokenizer(br.readLine());
+			sb.append(get(1, n, 1, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()))).append("\n");
+		}
+		System.out.print(sb);
+ 	}
 }
