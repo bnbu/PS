b4955cd7433af9ac4d8860b6478ed5b3c5a4a7de
jsh031613 <jsh031613@naver.com>
2023-06-13 19:42:37 +0900

    add explanation to fenwick tree range update & point query

--- template/Java/Data_Struct/Segment_Tree/FenwickTree_range_update_point_query.java
+++ template/Java/Data_Struct/Segment_Tree/FenwickTree_range_update_point_query.java
@@ -73,5 +73,12 @@ public class FenwickTree {
 // 구간 합 쿼리는 사용 불가
 // getSumFenwick(i)를 통해 i번째 위치의 값을 얻어올 수 있다.
 
+// 이는 펜윅트리가 배열 본연의 값을 저장하는게 아닌
+// 배열값은 따로 있고, imos 법에서의 imos 배열처럼
+// 변화값만을 저장해서, [1,K] 까지의 합은 결국 1부터 k까지의 변화값이 되어서
+// 이 구간합을 얻어오면 결국은 K의 최종 값이 되기 때문
+
+// 이 때문에 구간합 쿼리는 사용할 수 없게 된다.
+
 // https://nahwasa.com/entry/%ED%8E%9C%EC%9C%85-%ED%8A%B8%EB%A6%ACFenwick-tree-BIT-%EA%B8%B0%EB%B3%B8-2D-lazy-propagationrange-update-point-query-range-update-range-query#%EC%9D%91%EC%9A%A9_2_:_%EA%B5%AC%EA%B0%84_%EC%97%85%EB%8D%B0%EC%9D%B4%ED%8A%B8,_%EB%8B%A8%EC%9D%BC_%EA%B0%92_%ED%9A%8D%EB%93%9D
 // 저기 참고할 것
